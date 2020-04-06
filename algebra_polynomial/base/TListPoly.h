2b10897e930d8e577a031a642d5578961e6a7dc9
Anastasiya <Anastasiya.Zhurilova2yandex.ru>
2020-04-06 21:46:32 +0300

    полиномы и стэк

--- algebra_polynomial/base/TListPoly.h
+++ algebra_polynomial/base/TListPoly.h
@@ -1,8 +1,59 @@
-#pragma once
-class TListPoly
+#ifndef _TLISTPOLY_H_
+#define _TLISTPOLY_H_
+
+#include "TMonomial.h"
+#include <iostream>
+
+// À¿—— «¬≈ÕŒ-ÃŒÕŒÃ
+class TPLink
 {
 public:
-	TListPoly();
-	~TListPoly();
+	TMonom m;
+	TPLink* pNext;
+	TPLink() :pNext(nullptr) {}
+	~TPLink()
+	{
+		if (pNext != nullptr)
+		{
+			pNext = nullptr;
+		}
+	}
 };
 
+// À¿—— —œ»—Œ  ÃŒÕŒÃŒ¬
+class TPList
+{
+private:
+	TPLink* pFirst;
+	int size_list;
+public:
+	TPList();
+	TPList(const TPList& Plist);
+	~TPList();
+	TPLink* GetpFirst()
+	{
+		return pFirst;
+	}
+	void SetSizeL(const int& s)
+	{
+		size_list = s;
+	}
+	int GetSizeL()
+	{
+		return size_list;
+	}
+
+	void InsertLast(const TMonom& mon);
+	void TakeFirst();
+	TMonom GetMonPos(const int pos);
+	TPLink* Find(const int& pos);
+	TPLink* DeleteTheLink(TPLink* p);
+
+	TPList& operator=(const TPList& Plist);
+	bool operator==(const TPList& Plist) const;
+	bool operator!=(const TPList& Plist) const;
+
+	void SortPList();
+
+};
+#endif // !_TLISTPOLY_H_
